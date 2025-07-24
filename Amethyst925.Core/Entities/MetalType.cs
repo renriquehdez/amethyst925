7d8dada3cd6942cf925dc434d6970f932ada1263
Reynaldo E. Hernández (SV) <svhernandezrey@vitalialimentos.com>
2025-07-24 16:35:43 -0600

    Adición de entidad MetalType

---
+++ Amethyst925.Core/Entities/MetalType.cs
@@ -0,0 +1,26 @@
+﻿namespace Amethyst925.Core.Entities;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class MetalType
+{
+    /// <summary>
+    /// Código del Tipo de Metal
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Nombre del Tipo de Metal
+    /// </summary>
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Descripción del Tipo de Metal
+    /// </summary>
+    [MaxLength(250)]
+    public string Description { get; set; } = string.Empty;
+}
