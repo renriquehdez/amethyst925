e3c04f5266543cb5322e25c6f052207ced166698
Reynaldo E. Hernández (SV) <svhernandezrey@vitalialimentos.com>
2025-07-24 16:33:36 -0600

    Adición de entidad Branch

---
+++ Amethyst925.Core/Entities/Branch.cs
@@ -0,0 +1,38 @@
+﻿namespace Amethyst925.Core.Entities;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Branch
+{
+    /// <summary>
+    /// Código Sucursal
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Nombre de la Sucursal
+    /// </summary>
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Dirección de la Sucursal
+    /// </summary>
+    [MaxLength(250)]
+    public string Address { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Teléfono de la Sucursal
+    /// </summary>
+    [MaxLength(50)]
+    public string Phone { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Correo Electrónico
+    /// </summary>
+    [MaxLength(50)]
+    public string Email { get; set; } = string.Empty;
+}
