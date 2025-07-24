3c418b76fc91bd435f04622ffb585c0a9af307c9
Reynaldo E. Hernández (SV) <svhernandezrey@vitalialimentos.com>
2025-07-24 16:17:11 -0600

    Adición de proyecto Amethyst925.Windows

---
+++ Amethyst925.Windows/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Amethyst925.Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
