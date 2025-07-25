43df216c4d095b8dd609591c186355ed0b2161e4
Reynaldo E. Hernández (SV) <svhernandezrey@vitalialimentos.com>
2025-07-25 10:53:04 -0600

    Adición entidad Promotion

---
+++ Amethyst925.Core/Entities/Promotion.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Amethyst925.Core.Entities;
+
+public class Promotion
+{
+    /// <summary>
+    /// Código de Promoción
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Nombre de Promoción
+    /// </summary>
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Descripción de Promoción
+    /// </summary>
+    [MaxLength(250)]
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Porcentaje de Descuento
+    /// </summary>
+    public decimal DiscountPercentage { get; set; } // con 2 decimales
+
+    /// <summary>
+    /// Fecha Inicial del Descuento
+    /// </summary>
+    public DateTime StartDate { get; set; }
+
+    /// <summary>
+    /// Fecha Final del Descuento
+    /// </summary>
+    public DateTime EndDate { get; set; }
+
+    /// <summary>
+    /// Está Activa la Promoción
+    /// </summary>
+    public bool IsActive { get; set; }
+}
