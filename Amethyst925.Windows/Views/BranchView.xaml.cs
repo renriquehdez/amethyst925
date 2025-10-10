f642f86958a62f5bbb04c3c2507b985b4ce76c66
renriquehdez <renriquehdez@gmail.com>
2025-10-10 10:50:46 -0600

    Ajuste

---
+++ Amethyst925.Windows/Views/BranchView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Amethyst925.Services;
+using Amethyst925.Windows.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Amethyst925.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for BranchView.xaml
+    /// </summary>
+    public partial class BranchView : Window
+    {
+        public BranchView()
+        {
+            InitializeComponent();
+
+            DataContext = new BranchViewModel(
+                ((App)Application.Current).ServiceProvider.GetRequiredService<IBranchService>());
+        }
+    }
+}
