a2f50f6cc95226210bcb807f9fe5535e6a19a171
renriquehdez <renriquehdez@gmail.com>
2025-10-11 10:37:04 -0600

    Revert "Ajuste"
    This reverts commit f642f86958a62f5bbb04c3c2507b985b4ce76c66.

--- Amethyst925.Windows/App.xaml.cs
+++ Amethyst925.Windows/App.xaml.cs
@@ -1,49 +1,14 @@
-﻿namespace Amethyst925.Windows;
-
-using Amethyst925.Data;
-using Amethyst925.Services;
-using Amethyst925.Windows.Views;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Configuration;
+using System.Data;
 using System.Windows;
 
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace Amethyst925.Windows
 {
-    public IServiceProvider ServiceProvider { get; private set; }
-
-    public App()
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-        var services = new ServiceCollection();
-        ConfigureServices(services);
-        ServiceProvider = services.BuildServiceProvider();
     }
 
-    private void ConfigureServices(IServiceCollection services)
-    {
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Amethyst;Trusted_Connection=True;"));
-
-        services.AddScoped<IBranchService, BranchService>();
-
-        services.AddTransient<BranchView>(); // Registrar BranchView como servicio transitorio
-        services.AddSingleton<MainWindow>();
-    }
-
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-
-        // Asegurar que la base de datos esté creada
-        using var scope = ServiceProvider.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        dbContext.Database.EnsureCreated();
-
-        // Mostrar la ventana de sucursales (puedes cambiarlo a MainWindow si prefieres)
-        var branchView = ServiceProvider.GetRequiredService<BranchView>();
-        branchView.Show();
-    }
 }
